4a6ac348a1d171776e6b14e7ab63dbbc93565b13
andigunawan1123 <73595034+andigunawan1123@users.noreply.github.com>
2021-03-21 22:00:17 +0700

    Add files via upload

---
+++ 1/fungsi.pas
@@ -0,0 +1,17 @@
+program fungsi;
+procedure biodata;
+	var
+		umur,wa: Integer;
+		nama: string;
+	begin
+			write('nama :');
+			readln(nama);
+			write('umur :');
+			readln(umur);
+			write('wa :');
+			readln(wa);
+
+	end;
+begin
+		biodata;
+	end.
